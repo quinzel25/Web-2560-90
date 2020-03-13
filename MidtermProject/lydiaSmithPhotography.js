50ae365904eee90c035f4f11b504ab84e42ee3c1
Quinn Siebers <quinzel0325@gmail.com>
2020-03-13 16:28:32 -0500

    Midterm project running

---
+++ MidtermProject/lydiaSmithPhotography.js
@@ -0,0 +1,36 @@
+
+let contact = new Vue({
+
+    el: "#contact",
+    data: {
+        name: '',
+        email: '',
+        subject: '',
+        textArea: '',
+        errors: [],
+        successes: [],
+
+    },
+    methods: {
+        submit() {
+            this.errors = []
+            if (this.name && this.email && this.subject && this.textArea) {
+
+                console.log("Valid Entry")
+                this.name = ''
+                this.email = ''
+                this.subject = ''
+                this.textArea = ''
+                this.successes.push('Form submitted! Thank you for contacting me, I\'ll get back to you as soon as I can! :)')
+
+            } else {
+                this.errors.push('All fields are required.')
+            }
+
+
+        }
+
+    }
+
+
+})
