758d03dfbea5461c792cee127c1953fdf93b714f
quinz <quinzel0325@gmail.com>
2020-04-05 13:56:50 -0500

    All done with mid term project

--- MidtermProject/js/lydiaSmithPhotography.js
+++ MidtermProject/js/lydiaSmithPhotography.js
@@ -1,4 +1,4 @@
-
+// using Vue to add a responsive contact form that has validation. Of course there is no function since there is no server but it looks nice!
 let contact = new Vue({
 
     el: "#contact",
@@ -13,8 +13,10 @@ let contact = new Vue({
     },
     methods: {
         submit() {
+            // clears any messages when the button is clicked
             this.successes = []
             this.errors = []
+            // if all fields are filled out it will give the user a validation message and clear the text boxes
             if (this.name && this.email && this.subject && this.textArea) {
 
                 console.log("Valid Entry")
@@ -25,6 +27,8 @@ let contact = new Vue({
                 this.successes.push('Form submitted! Thank you for contacting me, I\'ll get back to you as soon as I can! :)')
 
             } else {
+                // validation message
+                console.log("Invalid Entry")
                 this.errors.push('All fields are required.')
             }
 
